635a29197b400e3f407d4cf931377809e6c406f8
Thorben Dahl <thorben@sjostrom.no>
2016-10-13 19:15:50 +0200

    Fix bug in which an empty set of matches resulted in an error
    Only the football screen was affected by this bug.

--- www/fedrelandet.php
+++ www/fedrelandet.php
@@ -83,81 +83,87 @@ try {
  // Used when debugging/developing
 //$extDocument = file_get_contents(DATADIR . '/fotballnedlasting.html');
 
- // Parse the document
-$document = new \DOMDocument();
-libxml_use_internal_errors(true);
-@$document->loadHTML($extDocument);
-$div_match = $document->getElementById('matches');
-if ($div_match === null) {
-    trigger_error('No element with ID "matches" was found on fotball.no - the page structure may have changed', E_USER_ERROR);
-}
-$table = $div_match->getElementsByTagName('table')->item(0);
-if ($table !== null && $table instanceof \DOMElement) {
-    // Set eventTable to the list of rows in the second table body (thus skipping the headings)
-    $tableBodies = $table->getElementsByTagName('tbody');
-    if ($tableBodies->length == 0) {
-        trigger_error('There are no table bodies in the matches table on fotball.no - the page structure may have changed', E_USER_ERROR);
+// Were there any matches found?
+if (!mb_ereg("Ingen kamper funnet", $extDocument)) {
+
+    // Parse the document
+    $document = new \DOMDocument();
+    libxml_use_internal_errors(true);
+    @$document->loadHTML($extDocument);
+    $div_match = $document->getElementById('matches');
+    if ($div_match === null) {
+        trigger_error('No element with ID "matches" was found on fotball.no - the page structure may have changed', E_USER_ERROR);
     }
-    $eventTable = $tableBodies->item(0);
-    if ($eventTable instanceof \DOMElement) {
-        $eventTable = $eventTable->getElementsByTagName('tr');
+    $table = $div_match->getElementsByTagName('table')->item(0);
+    if ($table !== null && $table instanceof \DOMElement) {
+        // Set eventTable to the list of rows in the second table body (thus skipping the headings)
+        $tableBodies = $table->getElementsByTagName('tbody');
+        if ($tableBodies->length == 0) {
+            trigger_error('There are no table bodies in the matches table on fotball.no - the page structure may have changed', E_USER_ERROR);
+        }
+        $eventTable = $tableBodies->item(0);
+        if ($eventTable instanceof \DOMElement) {
+            $eventTable = $eventTable->getElementsByTagName('tr');
+        } else {
+            trigger_error('Tablebody was not DOMElement - PHP behaviour may have been changed', E_USER_ERROR);
+        }
     } else {
-        trigger_error('Tablebody was not DOMElement - PHP behaviour may have been changed', E_USER_ERROR);
+        trigger_error("'#matches table' was not found on the external website, the page structure may have changed", E_USER_ERROR);
+        die();
+    }
+
+    // Enkel klasse som representerer en enkelt fotballkamp.
+    class Kamp
+    {
+        public $starttid;
+        public $hjemmelag;
+        public $bortelag;
+        public $avdeling;
+        public $bane;
+    }
+
+    $matches = [];
+    $needles = [
+        'Kunstgress',
+        'Fedrelandet ',
+        'Kg',
+        'Er'
+    ];
+    $replacements = [
+        '11-er',
+        '',
+        '',
+        '-er'
+    ];
+    // Foreach row in table
+    foreach ($eventTable as $event) {
+        if (!($event instanceof \DOMElement)) {
+            trigger_error('Table row was not instance of DOMElement, PHP behaviour may have changed', E_USER_ERROR);
+        }
+        $timeNode = $event->getElementsByTagName('td')->item(2);
+        $otherNodes = $event->getElementsByTagName('a');
+
+        // Transform from array to Kamp object
+        $thisMatch = new Kamp();
+
+        $thisMatch->starttid = DateTime::createFromFormat('H:i', $timeNode->textContent);
+
+        $thisMatch->hjemmelag = $otherNodes->item(0)->textContent;
+        $thisMatch->bortelag = $otherNodes->item(2)->textContent;
+
+
+        $thisMatch->avdeling = $otherNodes->item(4)->textContent;
+        $thisMatch->avdeling = mb_ereg_replace(' avd \d+', '', $thisMatch->avdeling);
+        $thisMatch->avdeling = mb_eregi_replace('([JG])(\d+)', '\1 \2', $thisMatch->avdeling);
+
+
+        $thisMatch->bane = str_replace($needles, $replacements, mb_convert_case($otherNodes->item(3)->textContent, MB_CASE_TITLE));
+        $matches[] = $thisMatch;
     }
 } else {
-    trigger_error("'#matches table' was not found on the external website, the page structure may have changed", E_USER_ERROR);
-    die();
+    // No matches
+    $matches = array();
 }
-
-// Enkel klasse som representerer en enkelt fotballkamp.
-class Kamp
-{
-    public $starttid;
-    public $hjemmelag;
-    public $bortelag;
-    public $avdeling;
-    public $bane;
-}
-
-$matches = [];
-$needles = [
-    'Kunstgress',
-    'Fedrelandet ',
-    'Kg',
-    'Er'
-];
-$replacements = [
-    '11-er',
-    '',
-    '',
-    '-er'
-];
-// Foreach row in table
-foreach ($eventTable as $event) {
-    if (!($event instanceof \DOMElement)) {
-        trigger_error('Table row was not instance of DOMElement, PHP behaviour may have changed', E_USER_ERROR);
-    }
-    $timeNode = $event->getElementsByTagName('td')->item(2);
-    $otherNodes = $event->getElementsByTagName('a');
-
-    // Transform from array to Kamp object
-    $thisMatch = new Kamp();
-
-    $thisMatch->starttid = DateTime::createFromFormat('H:i', $timeNode->textContent);
-
-    $thisMatch->hjemmelag = $otherNodes->item(0)->textContent;
-    $thisMatch->bortelag = $otherNodes->item(2)->textContent;
-
-
-    $thisMatch->avdeling = $otherNodes->item(4)->textContent;
-    $thisMatch->avdeling = mb_ereg_replace(' avd \d+', '', $thisMatch->avdeling);
-    $thisMatch->avdeling = mb_eregi_replace('([JG])(\d+)', '\1 \2', $thisMatch->avdeling);
-
-
-    $thisMatch->bane = str_replace($needles, $replacements, mb_convert_case($otherNodes->item(3)->textContent, MB_CASE_TITLE));
-    $matches[] = $thisMatch;
-}
-
 // Output
 $twig = Template::init();
 
